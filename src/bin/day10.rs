728f04a526df89d4f2f6063268b315c8cad408d7
erickmerchant <erickmerchant@gmail.com>
2022-12-11 01:38:11 -0500

    Day 10 - Part 1

---
+++ src/bin/day10.rs
@@ -0,0 +1,55 @@
+use std::{fs, str::Split};
+
+fn get_total(lines: Split<char>, mut cycles: Vec<i32>) -> i32 {
+    let mut total: i32 = 0;
+    let mut x_total: i32 = 1;
+    let mut current_cycle: i32 = 0;
+
+    let mut step = |x_total: i32| {
+        current_cycle += 1;
+
+        if let Some(next_cycle) = cycles.last() {
+            if next_cycle == &current_cycle {
+                total += x_total * current_cycle;
+
+                cycles.pop();
+            }
+        }
+    };
+
+    for line in lines {
+        if line.starts_with("addx") {
+            if let Some((_, x)) = line.split_once(' ') {
+                let x = x.parse::<i32>().unwrap();
+
+                step(x_total);
+
+                step(x_total);
+
+                x_total += x;
+            }
+        }
+
+        if line == "noop" {
+            step(x_total);
+        }
+    }
+
+    total
+}
+
+fn main() {
+    if let Ok(input) = fs::read_to_string("input/day10.txt") {
+        let lines = input.split('\n');
+        let mut cycles: Vec<i32> = Vec::new();
+        cycles.push(220);
+        cycles.push(180);
+        cycles.push(140);
+        cycles.push(100);
+        cycles.push(60);
+        cycles.push(20);
+        let total = get_total(lines.clone(), cycles);
+
+        println!("{total}");
+    }
+}
